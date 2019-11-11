939b484784f474b5a9b166a9d4ad4a1f1fbe779c
Romans Shvets <pomaxa@gmail.com>
2019-11-11 10:15:19 +0200

    create separate formType and API endpoint for application submit

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -5,7 +5,10 @@ namespace App\Controller;
 use App\Entity\Application;
 use App\Entity\Attender;
 use App\Entity\Happening;
+use App\Form\NewApplicationApiType;
+use App\Service\FileUploader;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -20,7 +23,7 @@ class ApiController extends AbstractController
     /**
      * @Route("/", name="api_upcomming_events")
      */
-    public function index(Request $request )
+    public function index(Request $request)
     {
 
         $limit = $request->get('limit', 10);
@@ -63,7 +66,7 @@ class ApiController extends AbstractController
         /** @var Application[] $applications */
         $applications = $this->getDoctrine()->getRepository(Application::class)->findByEvent($event);
         foreach ($applications as $app) {
-            if(!$app->getAttender() instanceof Attender) {
+            if (!$app->getAttender() instanceof Attender) {
                 continue;
             }
 
@@ -81,5 +84,79 @@ class ApiController extends AbstractController
                 'data' => $data,
             ]
         );
+    }
+
+
+    /** @Route("/apply/event/{eventId}", name="api_event_submit") */
+    public function submit($eventId, Request $request, FileUploader $fileUploader)
+    {
+        $happening = $this->getDoctrine()->getRepository(Happening::class)->find($eventId);
+        if (!$happening instanceof Happening) {
+            return new JsonResponse(
+                [
+                    'metadata' => [
+                        'route' => 'api_event_submit',
+
+                    ],
+                    'code' => 1,
+                    'data' => [
+                        'message_code' => 'happening_not_found',
+                        'message' => 'Error. There is no such active happening.',
+                    ],
+                ]
+            );
+        }
+        $application = new Application;
+        $application->setHappening($happening);
+        $application->setAttender(new Attender());
+
+        $form = $this->createForm(NewApplicationApiType::class, $application);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $application = $form->getData();
+
+            /** @var UploadedFile $avatarFile */
+            $avatarFile = $form['attender']['avatar']->getData();
+            if ($avatarFile) {
+                $avatarFilename = $fileUploader->upload($avatarFile);
+                $application->getAttender()->setAvatarFilename($avatarFilename);
+            }
+
+            $this->getDoctrine()->getManager()->persist($application);
+            $this->getDoctrine()->getManager()->flush();
+
+            return new JsonResponse(
+                [
+                    'metadata' => [
+                        'route' => 'api_event_submit',
+                    ],
+                    'code' => 0,
+                    'data' => [
+                        'message' => 'Your application successfully submitted ',
+                        'message_code' => 'application_submit_success',
+                    ],
+                ]
+            );
+        }
+
+
+        $response = new JsonResponse(
+            [
+                'metadata' => 'api_event_submit',
+                'code' => 2,
+                'data' => [
+                    'message' => '',
+                    'message_code' => 'some_error',
+                ]
+            ]
+        );
+        foreach ($form->getErrors() as $error) {
+            $response['data']['message'] .= $error->getMessage() . ' ' . PHP_EOL;
+        }
+
+        return $response;
     }
 }

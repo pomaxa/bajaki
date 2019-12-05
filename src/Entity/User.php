d0c7bdb3ef955cfbe15932da75cf90f4ea595345
Romans Shvets <pomaxa@gmail.com>
2019-12-06 00:11:29 +0200

    User entity upd

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -11,7 +11,6 @@ use Symfony\Component\Security\Core\User\UserInterface;
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
- * @ORM\Table(name="admin_user")
  */
 class User implements UserInterface
 {

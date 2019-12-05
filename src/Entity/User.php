63b7b47e76d5718e1f7606b7f1e95c9e2fae349e
Romans Shvets <pomaxa@gmail.com>
2019-12-06 01:47:16 +0200

    User Entity save

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -11,6 +11,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
+ * @ORM\Table(name="`user`")
  */
 class User implements UserInterface
 {

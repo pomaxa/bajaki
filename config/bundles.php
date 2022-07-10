dbeeef00ff380b282917721cf66376c7fe0e34c4
Romans Shvets <pomaxa@gmail.com>
2022-07-10 21:40:11 +0300

    add export functionality

--- config/bundles.php
+++ config/bundles.php
@@ -21,4 +21,5 @@ return [
     Sonata\Doctrine\Bridge\Symfony\Bundle\SonataDoctrineBundle::class => ['all' => true],
     Sonata\Form\Bridge\Symfony\SonataFormBundle::class => ['all' => true],
     Sonata\Twig\Bridge\Symfony\SonataTwigBundle::class => ['all' => true],
+    Sonata\Exporter\Bridge\Symfony\SonataExporterBundle::class => ['all' => true],
 ];
